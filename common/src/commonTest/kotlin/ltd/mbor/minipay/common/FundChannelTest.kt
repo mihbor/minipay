b527cde9104977a52ee1327e74a9827519cf6151
Michal Borowiecki <mihbor@users.noreply.github.com>
2023-07-28 19:53:43 +0100

    test fix fund channel with zero amount

--- common/src/commonTest/kotlin/ltd/mbor/minipay/common/FundChannelTest.kt
+++ common/src/commonTest/kotlin/ltd/mbor/minipay/common/FundChannelTest.kt
@@ -3,6 +3,7 @@ package ltd.mbor.minipay.common
 import com.benasher44.uuid.uuid4
 import com.ionspin.kotlin.bignum.decimal.BigDecimal.Companion.ONE
 import com.ionspin.kotlin.bignum.decimal.BigDecimal.Companion.TEN
+import com.ionspin.kotlin.bignum.decimal.BigDecimal.Companion.ZERO
 import kotlinx.coroutines.test.runTest
 import ltd.mbor.minipay.common.FundChannelEvent.*
 import ltd.mbor.minipay.common.model.Channel
@@ -14,6 +15,7 @@ import kotlin.test.assertNotNull
 
 @OptIn(kotlinx.coroutines.ExperimentalCoroutinesApi::class)
 class FundChannelTest {
+
   @Test
   fun prepareFundChannel() = runTest {
     //given
@@ -49,6 +51,47 @@ class FundChannelTest {
     assertEquals("my address", channel.my.address)
     assertEquals(keys2, channel.my.keys)
     assertEquals(ONE, channel.my.balance)
+    assertEquals("their address", channel.their.address)
+    assertEquals(keys, channel.their.keys)
+    assertEquals(TEN, channel.their.balance)
+    assertEquals(10, channel.timeLock)
+    assertEquals("multisig", channel.multiSigAddress)
+    assertEquals("eltoo", channel.eltooAddress)
+  }
+
+  @Test
+  fun prepareFundChannelWithZeroAmount() = runTest {
+    //given
+    val mds = SimulatedMDS()
+      .willReturn(fundChannel.fundingTx)
+      .willReturn(fundChannel.triggerTx)
+      .willReturn(fundChannel.settleTx)
+      .willReturn(fundChannel.exportTriggerTx)
+      .willReturn(fundChannel.exportSettleTx)
+      .willReturn(fundChannel.exportFundingTx)
+    val storage = SimulatedStorage.insertChannelWillReturn(uuid4())
+    val transport = SimulatedTransport()
+    val channelService = ChannelService(mds, storage, transport, mutableMapOf(), mutableListOf())
+    val events = mutableListOf<Pair<FundChannelEvent, Channel?>>()
+    //when
+    val channel = channelService.prepareFundChannel(ChannelInvite("0x00", "their address", TEN, keys, null), keys2, "my address", ZERO, 10, "multisig", "eltoo") { event, channel -> events.add(event to channel) }
+    //then
+    assertNotNull(channel)
+    assertEquals(1, transport.published.size)
+    assertEquals(channelKey(keys, "0x00"), transport.published.first().first)
+    assertEquals(
+      listOf("ACCEPTED", 10, keys2.trigger, keys2.update, keys2.settle, fundChannel.exportTriggerTxData, fundChannel.exportSettleTxData, fundChannel.exportFundingTxData, "my address").joinToString(";"),
+      transport.published.first().second
+    )
+    val eventsIterator = events.iterator()
+    assertEquals(FUNDING_TX_CREATED to null, eventsIterator.next())
+    assertEquals(TRIGGER_TX_SIGNED to null, eventsIterator.next())
+    assertEquals(SETTLEMENT_TX_SIGNED to null, eventsIterator.next())
+    assertEquals(CHANNEL_PERSISTED to channel, eventsIterator.next())
+    assertEquals(CHANNEL_PUBLISHED to channel, eventsIterator.next())
+    assertEquals("my address", channel.my.address)
+    assertEquals(keys2, channel.my.keys)
+    assertEquals(ZERO, channel.my.balance)
     assertEquals("their address", channel.their.address)
     assertEquals(keys, channel.their.keys)
     assertEquals(TEN, channel.their.balance)
